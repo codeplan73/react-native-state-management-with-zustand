ecd3bd82aac5d881bed8290823e702723b39521c
Emmanuel Omonzebaguan <codeplan73@gmail.com>
2024-05-04 14:28:44 +0100

    setup logger for productStore

--- store/cartStore.ts
+++ store/cartStore.ts
@@ -1,4 +1,31 @@
-import { create } from "zustand";
+import { create, StateCreator, StoreMutatorIdentifier } from "zustand";
+
+type Logger = <
+  T extends unknown,
+  Mps extends [StoreMutatorIdentifier, unknown][] = [],
+  Mcs extends [StoreMutatorIdentifier, unknown][] = []
+>(
+  f: StateCreator<T, Mps, Mcs>,
+  name?: string
+) => StateCreator<T, Mps, Mcs>;
+
+type LoggerImpl = <T extends unknown>(
+  f: StateCreator<T, [], []>,
+  name?: string
+) => StateCreator<T, [], []>;
+
+const loggerImpl: LoggerImpl = (f, name) => (set, get, store) => {
+  type T = ReturnType<typeof f>;
+  const loggedSet: typeof set = (...a) => {
+    set(...a);
+    console.log(...(name ? [`${name}:`] : []), get());
+  };
+  store.setState = loggedSet;
+
+  return f(loggedSet, get, store);
+};
+
+export const logger = loggerImpl as unknown as Logger;
 
 export interface CartState {
   products: Array<Product & { quantity: number }>;
@@ -9,50 +36,52 @@ export interface CartState {
   total: () => string;
 }
 
-const useCartStore = create<CartState>()((set, get) => ({
-  products: [],
-  addProduct: (product: Product) =>
-    set((state) => {
-      let hasProduct = false;
-      const products = state.products.map((p) => {
-        if (p.id === product.id) {
-          hasProduct = true;
-          return { ...p, quantity: p.quantity + 1 };
+const useCartStore = create<CartState>()(
+  logger((set, get) => ({
+    products: [],
+    addProduct: (product: Product) =>
+      set((state) => {
+        let hasProduct = false;
+        const products = state.products.map((p) => {
+          if (p.id === product.id) {
+            hasProduct = true;
+            return { ...p, quantity: p.quantity + 1 };
+          }
+          return p;
+        });
+
+        if (hasProduct) {
+          return { products };
         }
-        return p;
-      });
+        return { products: [...state.products, { ...product, quantity: 1 }] };
+      }),
 
-      if (hasProduct) {
-        return { products };
-      }
-      return { products: [...state.products, { ...product, quantity: 1 }] };
-    }),
+    reduceProduct: (product: Product) =>
+      set((state) => {
+        return {
+          products: state.products
+            .map((p) => {
+              if (p.id === product.id) {
+                return { ...p, quantity: p.quantity - 1 };
+              }
+              return p;
+            })
+            .filter((p) => p.quantity > 0),
+        };
+      }),
 
-  reduceProduct: (product: Product) =>
-    set((state) => {
-      return {
-        products: state.products
-          .map((p) => {
-            if (p.id === product.id) {
-              return { ...p, quantity: p.quantity - 1 };
-            }
-            return p;
-          })
-          .filter((p) => p.quantity > 0),
-      };
-    }),
+    clearCart: () =>
+      set(() => {
+        return { products: [] };
+      }),
 
-  clearCart: () =>
-    set(() => {
-      return { products: [] };
-    }),
+    items: () => get().products.reduce((acc, p) => acc + p.quantity, 0),
 
-  items: () => get().products.reduce((acc, p) => acc + p.quantity, 0),
-
-  total: () =>
-    get()
-      .products.reduce((acc, p) => acc + p.price * p.quantity, 0)
-      .toFixed(2),
-}));
+    total: () =>
+      get()
+        .products.reduce((acc, p) => acc + p.price * p.quantity, 0)
+        .toFixed(2),
+  }))
+);
 
 export default useCartStore;
